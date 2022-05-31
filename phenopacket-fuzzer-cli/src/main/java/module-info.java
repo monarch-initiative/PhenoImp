e8df6a3cc80e332a993d9e3bdb9fe1cfb4a580c5
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-05-31 18:17:20 -0400

    Set up `download` command for downloading the required data files.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacket-fuzzer-cli/src/main/java/module-info.java
+++ phenopacket-fuzzer-cli/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module org.monarchinitiative.phenopacket_fuzzer.cli {
     requires org.monarchinitiative.phenopacket_fuzzer.core;
+    requires org.monarchinitiative.biodownload;
     requires info.picocli;
+    requires org.slf4j;
+
+    opens org.monarchinitiative.phenopacket_fuzzer.cli.cmd to info.picocli;
 }
