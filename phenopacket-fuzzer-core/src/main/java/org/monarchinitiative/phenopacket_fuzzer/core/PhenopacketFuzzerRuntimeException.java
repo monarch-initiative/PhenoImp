8f38c182c698fee368754b37cd3eff0fd0c0834e
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-06-01 15:55:11 -0400

    Implement fuzzer for random removal of one of two variant interpretations in case of phenopacket with AR disease.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

---
+++ phenopacket-fuzzer-core/src/main/java/org/monarchinitiative/phenopacket_fuzzer/core/PhenopacketFuzzerRuntimeException.java
@@ -0,0 +1,24 @@
+package org.monarchinitiative.phenopacket_fuzzer.core;
+
+public class PhenopacketFuzzerRuntimeException extends RuntimeException {
+
+    public PhenopacketFuzzerRuntimeException() {
+        super();
+    }
+
+    public PhenopacketFuzzerRuntimeException(String message) {
+        super(message);
+    }
+
+    public PhenopacketFuzzerRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PhenopacketFuzzerRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PhenopacketFuzzerRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
--- phenopacket-fuzzer-core/src/test/java/org/monarchinitiative/phenopacket_fuzzer/core/TestBase.java
+++ phenopacket-fuzzer-core/src/test/java/org/monarchinitiative/phenopacket_fuzzer/core/TestBase.java
@@ -1,8 +1,13 @@
 package org.monarchinitiative.phenopacket_fuzzer.core;
 
+import org.monarchinitiative.phenol.annotations.formats.hpo.HpoDiseases;
+import org.monarchinitiative.phenol.annotations.io.hpo.HpoDiseaseLoader;
+import org.monarchinitiative.phenol.annotations.io.hpo.HpoDiseaseLoaderOptions;
+import org.monarchinitiative.phenol.annotations.io.hpo.HpoDiseaseLoaders;
 import org.monarchinitiative.phenol.io.OntologyLoader;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class TestBase {
@@ -11,8 +16,19 @@ public class TestBase {
 
     public static final Ontology HPO_TOY = loadToyHpo();
 
+    public static final HpoDiseases DISEASES = loadToyDiseases();
+
     private static Ontology loadToyHpo() {
         return OntologyLoader.loadOntology(TEST_BASE.resolve("hpo_toy.json").toFile());
+    }
+
+    private static HpoDiseases loadToyDiseases() {
+        HpoDiseaseLoader loader = HpoDiseaseLoaders.defaultLoader(TestBase.HPO_TOY, HpoDiseaseLoaderOptions.defaultOptions());
+        try {
+            return loader.load(TEST_BASE.resolve("phenotype.excerpt.hpoa"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
