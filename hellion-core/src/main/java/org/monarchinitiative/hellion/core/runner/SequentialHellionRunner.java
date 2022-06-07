5284efe44edaf8a1d87a365e3b84b8d97b4d431a
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-06-06 20:28:05 -0400

    Warn if no noise will be added.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- hellion-core/src/main/java/org/monarchinitiative/hellion/core/runner/SequentialHellionRunner.java
+++ hellion-core/src/main/java/org/monarchinitiative/hellion/core/runner/SequentialHellionRunner.java
@@ -3,15 +3,22 @@ package org.monarchinitiative.hellion.core.runner;
 import org.monarchinitiative.hellion.core.HellionRunner;
 import org.monarchinitiative.hellion.core.noise.PhenopacketNoise;
 import org.phenopackets.schema.v2.Phenopacket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SequentialHellionRunner implements HellionRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SequentialHellionRunner.class);
 
     private final List<PhenopacketNoise> noises;
 
     public SequentialHellionRunner(List<PhenopacketNoise> noise) {
-        this.noises = noise;
+        this.noises = Objects.requireNonNull(noise);
+        if (noise.isEmpty())
+            LOGGER.warn("No noise will be added!");
     }
 
     @Override
